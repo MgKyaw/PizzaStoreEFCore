ba8bbcaff6371eaca6fa29cdfd32c90c59f9e454
MgKyaw <mgkyaw20112@gmail.com>
2024-08-01 23:29:14 +0700

    feat: Add Get/id api

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ if(app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/pizzas", async (PizzaDb db) => db.Pizzas.ToListAsync());
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
 app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
 {
     await db.Pizzas.AddAsync(pizza);
